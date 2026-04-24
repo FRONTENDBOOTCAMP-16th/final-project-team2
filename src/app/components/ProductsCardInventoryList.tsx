cfcbd2c0bd44d4f3b215ab2f7df4e3d0287a73e1
songha-5 <shn0518@gmail.com>
2026-04-24 10:51:32 +0900

    feat: 불필요한 주석 제거

--- src/app/components/ProductsCardInventoryList.tsx
+++ src/app/components/ProductsCardInventoryList.tsx
@@ -3,7 +3,6 @@ import products from '@/data/dummyproducts.json';
 import ProductsCard from "./ProductsCard";
 
 export default function ProductsCardInventoryList() {
-  console.log('??')
   return (
     <ul className="flex [&>li]:border [&>li]:p-4 gap-4">
       <ProductsCard maxProducts={4} products={products} hasLike/>
