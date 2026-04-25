eb51b76ab38d60b57f8f9ed3296487f31e0dfdf1
ruiwaa <yeejii08@gmail.com>
2026-04-25 18:30:07 +0900

    feat: 찜한 상품 리스트 및 상품 없을 시 빈화면

--- src/app/mypage/consumer/wishlist/page.tsx
+++ src/app/mypage/consumer/wishlist/page.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import WishListItemsList from "./components/WishListItemsList";
 import TabFilter from "@/app/components/common/tabFilter";
+import { dummyOrders } from "@/data/dummyOrder";
 
 const CATEGORIES = [
   { id: "", label: "전체" },
@@ -9,12 +10,14 @@ const CATEGORIES = [
   { id: "paper", label: "노트/메모" },
 ];
 
+const HAS_WISH_PRODUCTS = dummyOrders.length > 0
+
 export default function WishlistPage() {
   const [selectedValue, setSelectedValue] = useState("");
   return (
-    <div className="max-w-4xl mx-auto p-6">
+    <div className="max-w-4xl m-auto mt-6 bg-white pt-6 px-6 pb-11.25 ">
       <h1 className="text-2xl font-bold mb-8 sr-only">찜한 상품</h1>
-      <div className="flex justify-between mb-16">
+      <div className="flex justify-between py-6">
         <TabFilter
           items={CATEGORIES}
           selectedValue={selectedValue}
@@ -23,16 +26,18 @@ export default function WishlistPage() {
         <label htmlFor="filter" className="sr-only">
           필터
         </label>
-        <select name="filter" id="filter" className="border-2 p-1 rounded-2xl">
+        <select name="filter" id="filter" className="border py-1 ">
           <option value="lastProduct">등록순</option>
           <option value="popularProduct">인기순</option>
           <option value="highPriceProduct">가격 높은 순</option>
           <option value="lowPriceProduct">가격 낮은 순</option>
         </select>
       </div>
-      <div className="grid grid-cols-2 md:grid-cols-3 gap-3">
+      {HAS_WISH_PRODUCTS ?  (<div className="grid grid-cols-2 md:grid-cols-3 gap-x-6  gap-y-15 ">
         <WishListItemsList />
-      </div>
+      </div>) : ( <div className="text-red-500 text-center pt-3">
+      <p>찜한 상품이 없습니다.</p> </div>)}
+     
     </div>
   );
 }
---
+++ src/types/productLike.ts
@@ -0,0 +1,6 @@
+export interface ProductLike {
+  id: string;       
+  user_id: string;     
+  product_id: string;
+  created_at: string; 
+}
