4261f72526a2fa7e55550e06ed613c6991c91438
ruiwaa <yeejii08@gmail.com>
2026-04-25 23:51:53 +0900

    feat: 찜하기 토글 버튼 추가

--- src/app/mypage/consumer/wishlist/components/WishListItemsList.tsx
+++ src/app/mypage/consumer/wishlist/components/WishListItemsList.tsx
@@ -1,6 +1,7 @@
 import { dummyOrders } from "@/data/dummyOrder";
 import Image from "next/image";
 import Link from "next/link";
+import LikeToggleButton from "../../../../components/common/LikeToggleButton";
 
 const MAX_COUNT = 9;
 const CATEGORY_TO_KOREAN: { writing: string; paper: string } = {
@@ -11,11 +12,14 @@ const CATEGORY_TO_KOREAN: { writing: string; paper: string } = {
 export default function WishListItemsList() {
   const wishlistItem = dummyOrders.slice(0, MAX_COUNT);
   return wishlistItem.map((item) => (
-    <div key={item.id} className="flex flex-row ">
-      <Link href={`/products/pen/${item.id}`} className="relative flex flex-col">
-      <div className="absolute top-0 left-0  bg-[#FF6B6B] text-white px-2 py-1 text-sm font-bold">
-      {item.discountRate}%
-    </div>
+    <div key={item.id} className="flex flex-col">
+      <Link
+        href={`/products/pen/${item.id}`}
+        className="relative flex flex-col"
+      >
+        <div className="absolute top-0 left-0  bg-[#FF6B6B] text-white px-2 py-1 text-sm font-bold">
+          {item.discountRate}%
+        </div>
         <Image
           width={282}
           height={282}
@@ -23,24 +27,28 @@ export default function WishListItemsList() {
           src={item.image}
           alt=""
         />
-        <div className="flex flex-col gap-2 pr-4 pt-4">
-          <h2 className="mt-2">{item.name}</h2>
+      </Link>
+      <div className="flex flex-col gap-2 pr-4 pt-4">
+        <h2 className="mt-2">{item.name}</h2>
+        <div className="flex justify-between">
           <p className="text-sm text-gray-400">
             {CATEGORY_TO_KOREAN[item.category]}
           </p>
 
-          <div className="flex gap-2">
-            {item.discountRate > 0 && (
-              <span className="text-red-500 font-bold text-sm">
-                {item.discountRate}%
-              </span>
-            )}
-            <span className="font-bold text-sm text-slate-800">
-              {item.price.toLocaleString()}원
-            </span>
-          </div>
+          <LikeToggleButton />
         </div>
-      </Link>
+
+        <div className="flex gap-2">
+          {item.discountRate > 0 && (
+            <span className="text-red-500 font-bold text-sm">
+              {item.discountRate}%
+            </span>
+          )}
+          <span className="font-bold text-sm text-slate-800">
+            {item.price.toLocaleString()}원
+          </span>
+        </div>
+      </div>
     </div>
   ));
 }
