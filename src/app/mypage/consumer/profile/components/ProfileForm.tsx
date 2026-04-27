713ca76a16e7c68dc092cde45e913c5d90bdd705
sasasak <geun02young@naver.com>
2026-04-27 17:46:45 +0900

    feat: 마이페이지 프로필 관리 퍼블리싱

---
+++ src/app/mypage/consumer/profile/components/ProfileForm.tsx
@@ -0,0 +1,53 @@
+import { useId } from "react";
+
+interface ProfileFormProps {
+  label: string;
+  name: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder?: string;
+  disabled?: boolean;
+  type?: string;
+  readOnly?: boolean;
+}
+
+export default function ProfileForm({
+  label,
+  name,
+  value,
+  onChange,
+  placeholder,
+  disabled = false,
+  type,
+  readOnly = false,
+}: ProfileFormProps) {
+  const id = useId();
+  const isNotEditable = disabled || readOnly;
+  return (
+    <div className="flex flex-col gap-2 w-full">
+      <label htmlFor={id} className="text-sm font-semibold text-gray-700 ml-1">
+        {label}
+      </label>
+
+      <input
+        id={id}
+        name={name}
+        type={type}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        disabled={disabled}
+        readOnly={readOnly}
+        className={`w-[672px] h-[50px] pl-4 border border-gray-300
+           
+          ${
+            isNotEditable
+              ? "bg-gray-50  text-gray-400 cursor-not-allowed"
+              : "bg-white  text-gray-800 focus:border-gray-400 focus:ring-1 focus:ring-gray-400"
+          }
+        
+        `}
+      />
+    </div>
+  );
+}
--- src/app/mypage/consumer/profile/page.tsx
+++ src/app/mypage/consumer/profile/page.tsx
@@ -1,3 +1,107 @@
-export default function Orders() {
-  return <h1>프로필 관리</h1>;
+"use client";
+
+import { useState } from "react";
+import ProfileForm from "./components/ProfileForm";
+import ImageUploader from "./components/ImageUploader";
+
+export default function Profile() {
+  const [isEditing, setIsEditing] = useState(false);
+  const [formData, setFormData] = useState({
+    profileImage: "",
+    name: "홍길동",
+    email: "user01@example.com",
+    nickname: "행쇼마켓너무좋아요",
+    phone: "010-1234-5678",
+    address: "서울시 강남구 테헤란로 123",
+    birthday: "1990-01-01",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  return (
+    <section className="p-8 bg-white mt-8">
+      <div className="flex justify-between items-center mb-6">
+        <h1 className="text-xl font-bold">프로필 수정</h1>
+        <button
+          onClick={() => setIsEditing(!isEditing)}
+          className="bg-red-400 text-white px-4 py-2"
+        >
+          {isEditing ? "저장하기" : "수정"}
+        </button>
+      </div>
+
+      <div className="flex flex-col gap-5">
+        <ImageUploader
+          label="프로필 이미지"
+          defaultImage={formData.profileImage}
+        />
+        <ProfileForm
+          label="이메일"
+          name="email"
+          type="email"
+          value={formData.email}
+          onChange={handleChange}
+          disabled={!isEditing}
+          readOnly={true}
+        />
+        <ProfileForm
+          label="이름"
+          name="name"
+          type="text"
+          value={formData.name}
+          onChange={handleChange}
+          disabled={!isEditing}
+          readOnly={true}
+        />
+        <ProfileForm
+          label="닉네임"
+          name="nickname"
+          type="text"
+          value={formData.nickname}
+          onChange={handleChange}
+          disabled={!isEditing}
+        />
+        <ProfileForm
+          label="휴대전화"
+          name="phone"
+          type="tel"
+          value={formData.phone}
+          onChange={handleChange}
+          disabled={!isEditing}
+        />
+        <ProfileForm
+          label="주소"
+          name="address"
+          type="text"
+          value={formData.address}
+          onChange={handleChange}
+          disabled={!isEditing}
+        />
+        <ProfileForm
+          label="생일"
+          name="birthday"
+          type="date"
+          value={formData.birthday}
+          onChange={handleChange}
+          disabled={!isEditing}
+          readOnly={true}
+        />
+        <button
+          type="button"
+          className="text-white w-[672px] h-[50px] bg-black cursor-pointer"
+        >
+          비밀번호 변경
+        </button>
+        <button
+          type="button"
+          className="text-white w-22 h-8 bg-red-700 cursor-pointer"
+        >
+          회원 탈퇴
+        </button>
+      </div>
+    </section>
+  );
 }
