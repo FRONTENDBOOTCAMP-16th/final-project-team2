347a420a7417c4e8fdb6636743340a1a97982a96
ruiwaa <yeejii08@gmail.com>
2026-04-27 23:58:48 +0900

    refactor: hover 시에 찜한 아이콘 스타일링 추가

--- src/app/components/common/LikeToggleButton.tsx
+++ src/app/components/common/LikeToggleButton.tsx
@@ -12,25 +12,29 @@ export default function LikeToggleButton() {
     setIsLike(nextLikeStatus);
 
     //추가/해체 모두 다 토스트 보여줘야함
-    setShowToast(true)
-
+    setShowToast(true);
   };
 
-  
   useEffect(() => {
     if (showToast) {
       const timer = setTimeout(() => {
         setShowToast(false);
-      }, 2000); 
+      }, 2000);
 
-      return () => clearTimeout(timer); 
+      return () => clearTimeout(timer);
     }
   }, [showToast]);
 
   return (
     // 버튼 누르면 찜한 상품에 추가되었다는 문구, 삭제가 되면 삭제되었다는 문구
     <>
-      <button onClick={handleLike}>{isLike ? (<Heart className="fill-red-500 text-red-500" />) : (<Heart/>)}</button>
+      <button onClick={handleLike}>
+        {isLike ? (
+          <Heart className="w-10 h-10 p-2 fill-red-500 text-red-500 transition-transform duration-200 hover:scale-130 " />
+        ) : (
+          <Heart className="w-10 h-10 p-2 hover:fill-pink-200" />
+        )}
+      </button>
 
       {showToast && (
         <div className="fixed bottom-10 left-1/2 -translate-x-1/2 z-20">
