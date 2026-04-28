ab0b2b797302952a1921fe494ef71f5c4949bef9
songha-5 <shn0518@gmail.com>
2026-04-28 10:40:15 +0900

    feat: 헤더 수정 - 쿠폰 레이아웃 추가 - 메뉴 축약 - 메뉴 아이콘 변경 - 메뉴 디자인 변경 - 호버 추가

--- src/app/components/Navi.tsx
+++ src/app/components/Navi.tsx
@@ -5,47 +5,41 @@ import NavList from "./NavList"
 
 
 const convenienceMenu = [
-  { name: "검색", onClick: () => console.log('검색 클릭'), icon: '/icon_search.png'},
-  { name: "쿠폰", href: "/coupon", icon: '/icon_coupon.png'},
-  { name: "마이페이지", href: "/myPage", icon: '/icon_mypage.png'},
-  { name: "장바구니", href: "/cart", icon: '/icon_cart.png'},
-]
-
-const userMenu = [
-  { name: "로그인", href: "/login"},
-  { name: "회원가입", href: "/signup"},
+  { name: "검색", onClick: () => console.log('검색 클릭'), icon: '🔍'},
+  { name: "쿠폰", href: "/coupon", icon: '🎫'},
+  { name: "마이페이지", href: "/myPage", icon: '👤'},
+  { name: "장바구니", href: "/cart", icon: '🛒'},
 ]
 
 const mainMenu = [
-  { name: "볼펜", href: "/ballPen"},
-  { name: "만년필", href: "/fountainPen"},
-  { name: "지우개", href: "/eraser"},
-  { name: "편지/카드", href: "/card"},
-  { name: "노트", href: "note"},
-  { name: "마스킹 테이프", href: "/tape"},
-  { name: "스티커", href: "sticker"},
-  { name: "스탬프/실링왁스", href: "/stamp"},
-  { name: "키링", href: "/keyring"},
+  { name: "필기구", href: "/writing"},
+  { name: "페이퍼", href: "/paper"},
+  { name: "다꾸/데코", href: "/deco"},
+  { name: "소품/액세서리", href: "/accessory"},
 ]
 
 export default function Header() {
   return (
-    <header className="top-0 w-full bg-white sticky z-2">
-      <div className="flex justify-between border-be">
+    <>
+      {/* 쿠폰 */}
+      <Link href={'/coupon'} className="flex px-8 py-2 bg-[#FF6B6B] text-sm text-white" >🎉 신규가입 시 5,000원 할인쿠폰 증정!</Link>
+
+      <header className="top-0 w-full bg-white sticky z-20 border-be border-be-[#2D3142]/9">
         {/* 편의 메뉴 */}
-        <NavList label="convenience-menu" items={convenienceMenu} />
+        <div className="flex max-w-7xl px-4 mx-auto md:px-4 py-4 justify-between items-center relative">
 
-        {/* 타이틀 */}
-        <h1>
-          <Link href="/">행쇼</Link>
-        </h1>
+          {/* 타이틀 */}
+          <h1>
+            <Link href="/" className="font-bold text-2xl">행쇼마켓</Link>
+          </h1>
 
-        {/* 유저 메뉴 */}
-        <NavList label="user-menu" items={userMenu} />
-      </div>
+          {/* 메뉴 리스트 */}
+          <NavList label="main-menu" items={mainMenu} className="md:absolute md:left-1/2 md:-translate-x-1/2" mainMenu/>
 
-      {/* 메인 메뉴 */}
-      <NavList label="main-menu" items={mainMenu} />
-    </header>
+          {/* 유저 메뉴 */}
+          <NavList label="convenience-menu" items={convenienceMenu} />
+        </div>
+      </header>
+    </>
   )
 }
