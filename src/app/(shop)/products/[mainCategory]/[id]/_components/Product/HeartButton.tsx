b58f28e2900548abb91bde66b68933de28003e5a
psy0821-k <p49417875@gmail.com>
2026-04-29 08:27:22 +0900

    fix: 상품상세 상품 설명 부분 컴포넌트 분리 및 옵션 영역 추가, 디자인 수정

---
+++ src/app/(shop)/products/[mainCategory]/[id]/_components/Product/HeartButton.tsx
@@ -0,0 +1,11 @@
+import { Heart } from 'lucide-react';
+
+const HeartButton = () => {
+  return (
+    <button className="w-14 aspect-square p-2 border flex justify-center items-center">
+      <Heart />
+    </button>
+  );
+};
+
+export default HeartButton;
