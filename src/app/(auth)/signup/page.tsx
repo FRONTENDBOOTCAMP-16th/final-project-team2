e09746cb90d2b83d1b9ef1bcb37fb61827b1e2a8
songha-5 <shn0518@gmail.com>
2026-04-30 17:52:08 +0900

    feat: 회원가입 레이아웃

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -5,14 +5,14 @@ import TypeRadioInput from "@/app/components/TypeRadioInput";
 export default function SignupPage() {
   return (
     <>
-      <div className="bg-white w-full p-10 sm:w-160 transition-all">
-        <div className="text-lg">
-          <strong>회원가입</strong>
+      <div className=" bg-white w-full p-10 sm:w-160 transition-all rounded-2xl shadow-md shadow-[#c7c7c7]">
+        <div className="text-2xl text-center border-be pbe-9 border-[#e0e0e0]">
+          <strong className="text-[#575A68]">회원가입</strong>
         </div>
 
         <form action="">
           {/* 계정 타입 */}
-          <div className="flex gap-2 mbs-10">
+          <div className="flex gap-2 mbs-6">
             <TypeRadioInput
               label="소비자"
               name="signup"
@@ -26,7 +26,7 @@ export default function SignupPage() {
           </div>
 
           {/* 아이디 패스워드 */}
-          <div className="flex flex-col gap-5 mbs-8">
+          <div className="flex flex-col gap-5 mbs-4">
             <InputBox type="text" label="이름" name="signup-name" placeholder="이름을 입력하세요"/>          
             <InputBox type="text" label="이메일" name="signup-email" placeholder="이메일을 입력하세요"/>          
             <InputBox type="text" label="핸드폰 번호" name="signup-phone" placeholder="핸드폰 번호를 입력하세요"/>          
@@ -34,15 +34,15 @@ export default function SignupPage() {
             <InputBox type="text" label="비밀번호 재확인" name="signup-check-password" placeholder="비밀 번호를 재입력하세요"/>          
           </div>
 
-          <div className="mbs-8">
+          <div className="mbs-6 border-be pbe-6 border-[#e0e0e0]">
             <CheckeBox name="login-terms" label="이용약관 동의(필수)" />
           </div>
 
-          <button type="submit" className="w-full py-4 mbs-4 bg-black text-white cursor-pointer">회원가입</button>
+          <button type="submit" className="w-full py-4 mbs-12 text-white bg-[#FF6B6B] rounded font-bold cursor-pointer">회원가입</button>
         </form>
 
       </div>
-      <strong className="mbs-4 text-red-600 text-center">현 사이트는 실제로 운영되는 페이지가 아닙니다.<br />개인정보를 넣지 말아주세요</strong>
+      <strong className="mbs-12 text-red-600 text-center">현 사이트는 실제로 운영되는 페이지가 아닙니다.<br />개인정보를 넣지 말아주세요</strong>
     </>
   )
 }
