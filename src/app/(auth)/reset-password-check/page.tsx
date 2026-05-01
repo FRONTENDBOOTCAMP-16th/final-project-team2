001c29c4f9aa6a510433641688c8017918f665e3
songha-5 <shn0518@gmail.com>
2026-05-01 11:36:03 +0900

    feat: 비밀번호 체크 페이지 aria-live 추가

--- src/app/(auth)/reset-password-check/page.tsx
+++ src/app/(auth)/reset-password-check/page.tsx
@@ -1,8 +1,7 @@
 import InputBox from "@/app/components/InputBox"
-import Link from "next/link"
 
 export default function ResetPasswordCheckPage() {
-  const errorMessage = 'a'
+  const errorMessage = '일치하는 아이디가 없습니다'
 
   return (
     <div className=" bg-white w-full p-10 sm:w-160 transition-all rounded-2xl shadow-md shadow-[#c7c7c7]">
@@ -18,7 +17,7 @@ export default function ResetPasswordCheckPage() {
         </div>
         <p className="pbs-4 text-center text-red-500 border-bs border-[#e0e0e0]">{errorMessage || '\u00A0'}</p>
 
-        <Link href="/reset-password-check/reset-password" className="text-center w-full py-4 mbs-12 text-white bg-[#FF6B6B] rounded font-bold cursor-pointer">확인</Link>
+        <button type="button" className="text-center w-full py-4 mbs-12 text-white bg-[#FF6B6B] rounded font-bold cursor-pointer">확인</button>
       </form>
     </div>
   )
--- src/app/(auth)/reset-password-check/reset-password/page.tsx
+++ src/app/(auth)/reset-password-check/reset-password/page.tsx
@@ -12,7 +12,7 @@ export default function ResetPasswordPage() {
           <InputBox type="password" label="비밀번호 입력" name="reset-password" placeholder="비밀번호를 입력하세요"/>          
           <InputBox type="password" label="비밀번호 재입력" name="reset-check-password" placeholder="비밀번호를 재입력하세요"/>          
         </div>
-        <p className="pbs-4 border-bs border-[#e0e0e0] text-center text-red-500">비밀번호가 동일하지 않습니다.</p>
+        <p aria-live="polite" className="pbs-4 border-bs border-[#e0e0e0] text-center text-red-500">비밀번호가 동일하지 않습니다.</p>
 
         <button type="submit" className="w-full py-4 mbs-12 text-white bg-[#FF6B6B] rounded font-bold cursor-pointer">메인페이지로 이동</button>
       </form>
