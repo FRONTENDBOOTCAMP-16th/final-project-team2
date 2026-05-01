9ad7726b1988a029db6c70e0e6cbcf28d6fac6b8
songha-5 <shn0518@gmail.com>
2026-05-01 11:12:01 +0900

    feat: 로그인 디자인

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -5,55 +5,43 @@ import Link from "next/link";
 
 export default function LoginPage() {
   return (
-    <div className="bg-white w-full p-10 sm:w-160 transition-all">
-      <div className="text-center">
-        <strong>환영합니다</strong>
-        <p className="mbs-2">계정에 로그인하세요</p>
+    <div className="bg-white w-full p-10 sm:w-160 transition-all rounded-2xl shadow-md shadow-[#c7c7c7]">
+      <div className="text-2xl text-center border-be pbe-9 border-[#e0e0e0]">
+        <strong className="text-[#575A68]">환영합니다</strong>
+        <p className="text-sm mbs-1 text-[#575A68]">계정에 로그인하세요</p>
       </div>
 
-      <form action="" className="mbs-10">
+      <form action="" className="flex flex-col gap-2 mbs-10 pb-7">
         {/* 계정 타입 */}
         <div className="flex gap-2">
-          <TypeRadioInput
-            label="소비자"
-            name="login"
-            value="USER"
-          />
-          <TypeRadioInput
-            label="판매자"
-            name="login"
-            value="BUSINSS"
-          />
-          <TypeRadioInput
-            label="관리자"
-            name="login"
-            value="ADMIN"
-          />
+          <TypeRadioInput label="소비자" name="login" value="USER" />
+          <TypeRadioInput label="판매자" name="login" value="BUSINSS" />
+          <TypeRadioInput label="관리자" name="login" value="ADMIN" />
         </div>
 
         {/* 아이디 패스워드 */}
-        <div className="flex flex-col gap-5 mbs-8">
+        <div className="flex flex-col gap-2 mbs-2">
           <InputBox type="text" label="아이디" name="login-id" placeholder="이메일을 입력하세요"/>          
           <InputBox type="password" label="패스워드" name="login-password" placeholder="비밀번호를 입력하세요"/>          
         </div>
 
         {/* 로그인 서브 */}
-        <div className="grid grid-cols-1 mbs-4">
+        <div className="grid grid-cols-1 mbs-2 pbe-9 border-be border-[#e0e0e0]">
           <CheckeBox name="login-stay" label="로그인 상태유지" />
-          <Link href="/signup" className="text-gray-600 row-start-1 col-start-2 text-right">회원가입</Link>
-          <Link href="/find-id" className="text-gray-600 row-start-2 col-start-2 text-right">아이디 찾기</Link>
-          <Link href="/reset-password-check" className="text-gray-600 row-start-3 col-start-2 text-right">비밀번호 재설정</Link>
+          <Link href="/signup" className="text-[#575A68] row-start-1 col-start-2 text-right">회원가입</Link>
+          <Link href="/find-id" className="text-[#575A68] row-start-2 col-start-2 text-right">아이디 찾기</Link>
+          <Link href="/reset-password-check" className="text-[#575A68] row-start-3 col-start-2 text-right">비밀번호 재설정</Link>
         </div>
 
         {/* 로그인버튼 */}
-        <button type="submit" className="w-full py-4 mbs-4 bg-gray-200 cursor-pointer">로그인 버튼</button>
+        <button type="submit" className="w-full py-4 mbs-12 text-white bg-[#FF6B6B] rounded font-bold cursor-pointer">로그인 버튼</button>
       </form>
 
       {/* 간편로그인 */}
-      <div className="relative border-bs mbs-12">
-        <span className="absolute left-1/2 -translate-1/2 bg-white px-6">간편로그인</span>
+      <div className="relative border-bs border-[#e0e0e0]">
+        <span className="absolute left-1/2 -translate-1/2 bg-white text-[#575A68] px-6">간편로그인</span>
         {/* 카카오로그인 */}
-        <button type="submit" className="w-full py-4 mbs-7 bg-gray-200 cursor-pointer">카카오 로그인</button>
+        <button type="submit" className="w-full py-4 mbs-7 text-black bg-[#FEE500] rounded font-bold cursor-pointer">카카오 로그인</button>
       </div>
     </div>
   )
