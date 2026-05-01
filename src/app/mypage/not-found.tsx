9c8fa7effc59237f2199835174a97b23e67cabd3
ruiwaa <yeejii08@gmail.com>
2026-05-01 18:46:35 +0900

    feat: app 404 페이지 추가

--- src/app/mypage/not-found.tsx
+++
@@ -1,9 +0,0 @@
-export default function notFound() {
-  return (
-    <section className="w-full min-h-screen bg-[#FFF8F3]">
-      <div className="bg-white">
-        <p>페이지를 찾을 수 없습니다.</p>
-      </div>
-    </section>
-  );
-}
