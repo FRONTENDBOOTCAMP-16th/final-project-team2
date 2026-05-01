54492a62a1feed201050320b78da97f21576d9c2
songha-5 <shn0518@gmail.com>
2026-05-01 10:22:31 +0900

    feat: any타입 수정

--- src/app/components/MainList.tsx
+++ src/app/components/MainList.tsx
@@ -27,7 +27,7 @@ const productList =[
 ]
 
 // 특정 기준에 따라 상품을 리스트업
-const sortedList = (products: any[], key: string) => {
+const sortedList = <T extends Record<string, any>>(products: T[], key: keyof T) => {
   return products.toSorted((a, b) => b[key] - a[key])
 }
 
