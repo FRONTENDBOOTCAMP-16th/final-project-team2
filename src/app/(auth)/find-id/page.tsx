d57ce8b6cd952a35ab060cc1c0e654fbc7c9888f
songha-5 <shn0518@gmail.com>
2026-05-01 11:35:46 +0900

    feat: 아이디 찾기 페이지 / 결과페이지 디자인

--- src/app/(auth)/find-id/page.tsx
+++ src/app/(auth)/find-id/page.tsx
@@ -1,25 +1,23 @@
 import InputBox from "@/app/components/InputBox";
-import Link from "next/link";
 
 export default function FIndIdPage() {
+  const errorMessage = '아이디가 존재하지 않습니다.'
+
   return (
-    <div className="bg-white w-full p-10 sm:w-160 transition-all">
-      <div>
-        <strong>아이디 찾기</strong>
+    <div className="bg-white w-full p-10 sm:w-160 transition-all rounded-2xl shadow-md shadow-[#c7c7c7]">
+      <div className="text-2xl text-center border-be pbe-9 border-[#e0e0e0]">
+        <strong className="text-[#575A68]">아이디 찾기</strong>
       </div>
 
       <form action="">
-        <div className="flex flex-col gap-5 mbs-8">
+        <div className="flex flex-col gap-2 mbs-10">
           <InputBox type="text" label="이름" name="find-id-name" placeholder="이름을 입력하세요"/>
           <InputBox type="text" label="이메일" name="find-id-email" placeholder="이메일을 입력하세요"/>
           <InputBox type="text" label="핸드폰 번호" name="find-id-phone" placeholder="핸드폰 번호를 입력하세요" />
         </div>
-        <p className="mbs-2 text-center text-red-500 invisible">아이디가 존재하지 않습니다</p>
-
+        <p aria-live="polite" className="border-be border-[#e0e0e0] pbe-12 mbs-2 text-center text-red-500">{errorMessage || '\u00A0'}</p>
         
-        {/* 실제 사용은 button으로 사용예정 연결확인을 위해 Link로 임시 연결*/}
-        <Link href={'/find-id/find-id-result'} className="block text-center w-full py-4 mbs-8 bg-gray-200 cursor-pointer">아이디 찾기</Link>
-        {/* <button type="submit" className="w-full py-4 mbs-8 bg-gray-200 cursor-pointer">아이디 찾기</button> */}
+        <button type="submit" className="text-center w-full py-4 mbs-12 text-white bg-[#FF6B6B] rounded font-bold cursor-pointer">아이디 찾기</button>
       </form>
     </div>
   )
