d00bdce4b176da60d52a27721b4d6bff0c6ac5b0
psy0821-k <p49417875@gmail.com>
2026-05-01 23:58:55 +0900

    fix: 정렬기능 url 반환 에러 수정

--- src/app/(shop)/products/[mainCategory]/_component/Sort.tsx
+++ src/app/(shop)/products/[mainCategory]/_component/Sort.tsx
@@ -1,13 +1,13 @@
 'use client';
-
-import { useRouter, useSearchParams } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import { CategoryType } from '../lib/category';
 
 type SortProps = {
   mainCategory: CategoryType;
 };
 
-const Sort = (mainCategory: SortProps) => {
+export default function Sort({ mainCategory }: SortProps) {
   const router = useRouter();
   const searchParams = useSearchParams();
 
@@ -21,19 +21,11 @@ const Sort = (mainCategory: SortProps) => {
   };
 
   return (
-    <div>
-      <label htmlFor="sort" className="sr-only">
-        정렬
-      </label>
-
-      <select id="sort" name="sort" onChange={handleChange} className="border w-50 h-9 px-3">
-        <option value="latest">최신순</option>
-        <option value="popular">인기순</option>
-        <option value="highPrice">가격 높은 순</option>
-        <option value="lowPrice">가격 낮은 순</option>
-      </select>
-    </div>
+    <select id="sort" name="sort" onChange={handleChange} className="border w-50 h-9 px-3">
+      <option value="latest">최신순</option>
+      <option value="popular">인기순</option>
+      <option value="highPrice">가격 높은 순</option>
+      <option value="lowPrice">가격 낮은 순</option>
+    </select>
   );
-};
-
-export default Sort;
+}
--- src/app/(shop)/products/[mainCategory]/page.tsx
+++ src/app/(shop)/products/[mainCategory]/page.tsx
@@ -3,9 +3,11 @@ import ProductArea from './_component/ProductArea';
 import Sort from './_component/Sort';
 import { getProducts } from '@/api/getProducts';
 import BreadCrumble from './_component/BreadCrumble';
+import FilterCategory from './_component/filterCategory';
+import { CATEGORY_MAP, CategoryType } from './lib/category';
 type Product = {
   params: Promise<{
-    mainCategory: string;
+    mainCategory: CategoryType;
   }>;
   searchParams: Promise<{
     category?: string;
@@ -31,14 +33,15 @@ const ProductListPage = async ({ params, searchParams }: Product) => {
 
   return (
     <div className="mt-5 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      <BreadCrumble categoryMap={data.category} />
-
+      <BreadCrumble categoryMap={CATEGORY_MAP[mainCategory].label} />
       <div className="text-center">
-        <p className="text-base font-semibold text-gray-600">장인은 도구탓을 합니다</p>
+        <h1 className="text-4xl font-bold mt-3">{keyword ? keyword : CATEGORY_MAP[mainCategory].label}</h1>
+        <p className="text-base mt-3 font-semibold text-gray-600">장인은 도구탓을 합니다</p>
       </div>
 
       <div className="flex justify-between mb-16 mt-18">
-        <Sort />
+        <FilterCategory mainCategory={mainCategory} sort={sort} category={keyword} />
+        <Sort mainCategory={mainCategory} />
       </div>
 
       <main>
