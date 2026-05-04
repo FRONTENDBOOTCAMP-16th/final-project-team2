49453f345f69d6a7ad4d9d1dcece361784467f0a
songha-5 <shn0518@gmail.com>
2026-05-04 10:49:51 +0900

    feat: 불필요한 zod 정리

--- src/app/lib/ahth.ts
+++ src/app/lib/ahth.ts
@@ -4,20 +4,18 @@ import { z } from 'zod'
 const roleSchema = z.enum(['USER', 'BUSINESS', 'ADMIN'], '유저 타입을 선택해주세요')
 const emailSchema = z.email('유효한 이메일이 아닙니다')
 const nameSchema = z.string('이름은 문자 값이여야 합니다').min(2, '이름은 2글자 이상의 문자열이여야합니다')
-const phoneSchema = z.string('사용자 번호는 문자여야합니다').regex(/^010\d{4}\d{4}/, '전화번호 형식이 올바르지 않습니다. (예: 010-0000-0000)')
+const phoneSchema = z.string('사용자 번호는 문자여야합니다').regex(/^010-\d{4}-\d{4}/, '전화번호 형식이 올바르지 않습니다. (예: 010-0000-0000)')
 const passwordSchema = z.string('유효한 비밀번호가 아닙니다').min(8, '비밀번호는 8자리 이상이여야 합니다').regex(/^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*?_]).{8,}$/, '영어, 숫자, 특수문자(!@#$%^&*?_)가 포함 8글자가 들어가야 합니다')
-
+const passwordConfirm = z.string().min(1, '비밀번호 확인을 입력해주세요')
 
 // 회원가입 베이스
 const baseSignupSchema = z.object({
-  role: roleSchema.refine((val) => !!val, {
-    message: '권한을 선택해주세요',
-  }),
+  role: roleSchema,
   email: emailSchema,
   name: nameSchema,
   phone: phoneSchema,
   password: passwordSchema,
-  confirmPassword: z.string().min(1, '비밀번호 확인을 입력해주세요')
+  confirmPassword: passwordConfirm
 })
 
 // 실제 사용되는 회원가입 스키마
@@ -25,8 +23,8 @@ export const signupSchema = baseSignupSchema.check((checkValue) => {
   // password, confirmPassword를 타입 지정
   const data = checkValue.value as { password: string; confirmPassword: string }
 
-  // 방어코드 + 값이 일치하지 않을때 메세지 출력
-  if (data.password && data.confirmPassword && data.password !== data.confirmPassword) {
+  // 값이 일치하지 않을때 메세지 출력
+  if (data.password !== data.confirmPassword) {
     checkValue.issues.push({
       code: 'custom',
       message: '비밀번호가 일치하지 않습니다.',
@@ -39,9 +37,7 @@ export const signupSchema = baseSignupSchema.check((checkValue) => {
 
 // 로그인 스키마
 export const loginSchema = z.object({
-  role: roleSchema.refine((val) => !!val, {
-    message: '권한을 선택해주세요',
-  }),
+  role: roleSchema,
   email: emailSchema,
   password: passwordSchema
 })
