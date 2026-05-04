706133655c6066c4b7bff3ded684c6c899580226
sasasak <geun02young@naver.com>
2026-05-04 15:38:22 +0900

    feat: 마이페이지 쿠폰 리스트 조립 - 임시 더미 데이터를 활용한 데이터 매핑 및 레이아웃 작성 - 데이터가 없을 경우에 대한 예외 처리 UI 추가

--- src/app/mypage/consumer/coupons/page.tsx
+++ src/app/mypage/consumer/coupons/page.tsx
@@ -1,3 +1,18 @@
-export default function Coupons() {
-  return <h1>쿠폰 관리</h1>;
+import { dummyUserCoupons } from "@/data/dummyCouponLists";
+import CouponItem from "./components/CouponItem";
+
+export default function Coupon() {
+  return (
+    <div className="flex flex-col gap-4 w-full py-12 px-18 bg-white">
+      {dummyUserCoupons.length > 0 ? (
+        dummyUserCoupons.map((userCoupon) => (
+          <CouponItem key={userCoupon.id} userCoupon={userCoupon} />
+        ))
+      ) : (
+        <p className="text-gray-500 text-center py-20">
+          사용 가능한 쿠폰이 없습니다.
+        </p>
+      )}
+    </div>
+  );
 }
