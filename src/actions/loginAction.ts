b300aaad093a84ae7a67928cece2263f7c344cbc
songha-5 <shn0518@gmail.com>
2026-05-06 15:21:24 +0900

    feat: 로그인 기능 - 로그인 상태 유지 기능 추가 - 로그인 email,password,role확인 후 로그인

--- src/actions/loginAction.ts
+++ src/actions/loginAction.ts
@@ -3,7 +3,7 @@
 import { loginSchema } from "@/app/lib/auth"
 import { authAction } from "./auth.actions"
 import { redirect } from "next/navigation"
-import { supabase } from "@/app/lib/supabase"
+import { createClient } from "../../utils/supabase/server"
 
 interface LoginStatus {
   errors: Record<string, string[]> | null
@@ -22,16 +22,39 @@ export const loginAction = async (_: unknown, formData: FormData): Promise<Login
 
   // 객체 변경 및 로그인지속 여부
   const objectForm = Object.fromEntries(formData) as Record<string, string>
-  const keepLogin = objectForm.keeplogin === 'on'
 
+  const isKeepLogin = objectForm.keeplogin === 'on'
+  const supabase = await createClient(isKeepLogin)
 
   // 아이디 비밀번호 체크
-  const { error } = await supabase.auth.signInWithPassword({
+  const { data: authData, error: authError } = await supabase.auth.signInWithPassword({
     email: objectForm.email,
     password: objectForm.password
   })
 
-  if (error) return { errors: { root: ['이메일 또는 비밀번호가 올바르지 않습니다'] } }
+  if (authError) {
+    return { 
+      errors: { root: ['이메일, 비밀번호 또는 타입이 올바르지 않습니다'] },
+      email: objectForm.email,
+      password: objectForm.password,
+      role: objectForm.role
+    }
+  }
   
+  // 로그인 시 타입확인
+  const { data: roleData, error: roleError } = await supabase.from('users').select('role').eq('id', authData.user?.id).single()
+
+  if (roleData?.role !== objectForm.role) {
+    // 타입이 다를시 로그아웃
+    await supabase.auth.signOut()
+    
+    return {
+      errors: { root: ['선택하신 타입이 일치하지 않습니다'] },
+      email: objectForm.email,
+      password: objectForm.password,
+      role: objectForm.role
+    }
+  }
+
   redirect('/')
 }
