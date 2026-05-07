81128fac3f3ff956579dabba6d3f4456d9ee126b
psy0821-k <p49417875@gmail.com>
2026-05-07 15:46:32 +0900

    chore: bun 재설치

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   cacheComponents: true,
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'xgiayrmzgokjzwwzivzi.supabase.co',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
