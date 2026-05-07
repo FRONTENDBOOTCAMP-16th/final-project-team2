22a86c486d76d4a3f96f5b08080598791cafbf26
ruiwaa <yeejii08@gmail.com>
2026-05-07 16:40:20 +0900

    feat: supabase 연동한 찜한 상품 페이지 1차 작업 완료 - 데이터 가져오는 로직 완성 - 해당 데이터 탭 필터링 - 해당 데이터 정렬 기능 - 아직 실제 supabase와 연동이 안됨 (작업 진행 중)

--- src/app/mypage/consumer/wishlist/components/WishListItemsList.tsx
+++ src/app/mypage/consumer/wishlist/components/WishListItemsList.tsx
@@ -1,28 +1,143 @@
 "use client";
 
 import WishListItemCard from "./WishListItemsCard";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TabFilter from "./tabFilter";
 import Pagination from "@/app/mypage/seller/delivery/components/Pagination";
 import { usePagination } from "@/hooks/usePagination";
-import { dummyOrderItems } from "@/data/dummyOrder";
+import { createClient } from "../../../../../../utils/supabase/client";
+import { ProductLike } from "@/app/lib/productLike";
+
+// 카드 미리보기 용 타입
+type ProductPreview = {
+  id: string;
+  name: string;
+  thumbnail_image: string;
+  price: number;
+  discount_rate: number;
+
+  product_categories: {
+    categories: {
+      id: string;
+      name: string;
+    };
+  }[];
+};
+
+// 기본 찜한 상품 타입 & join 용 타입 추가
+export type ProductLikeWithProduct = ProductLike & {
+  products: ProductPreview;
+};
 
 const CATEGORIES = [
   { id: "", label: "전체" },
-  { id: "writing", label: "필기구" },
-  { id: "paper", label: "노트/메모" },
+  { id: "필기구", label: "필기구" },
+  { id: "노트/메모", label: "노트/메모" },
 ];
 
+const supabase = createClient();
+
 export default function WishListItemsList() {
+  const [items, setItems] = useState<ProductLikeWithProduct[]>([]);
   const [selectedValue, setSelectedValue] = useState("");
-  const [filterValue, setFilterValue] = useState([...dummyOrderItems]);
+  const [sortValue, setSortValue] = useState("lastProduct");
 
+  const filteredItems = items.filter((item) => {
+    if (!selectedValue) return true;
+
+    // 여러 카테고리를 찾아주기
+    return item.products.product_categories.some(
+      (pc) => pc.categories.name === selectedValue,
+    );
+  });
+
+  // 데이터 가져오기
+  useEffect(() => {
+    const fetchLikes = async () => {
+      const {
+        data: { user },
+        error: userError,
+      } = await supabase.auth.getUser();
+
+      if (userError) {
+        console.error(userError);
+        return;
+      }
+
+      if (!user) {
+        console.log("로그인된 유저 없음");
+        return;
+      }
+
+      const { data, error } = await supabase
+        .from("product_likes")
+        .select(
+          `
+        id,
+        created_at,
+        user_id,
+        product_id,
+        products (
+          id,
+          name,
+          thumbnail_image,
+          price,
+          discount_rate,
+          product_categories (
+            categories (
+              id,
+              name
+            )
+          )
+        )
+      `,
+        )
+
+        .returns<ProductLikeWithProduct[]>();
+
+      console.log(data);
+
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      setItems(data ?? []);
+    };
+
+    fetchLikes();
+  }, []);
+
+  // 정렬할 아이템
+  const sortedItems = filteredItems.toSorted((a, b) => {
+    switch (sortValue) {
+      case "highPriceProduct":
+        return b.products.price - a.products.price;
+
+      case "lowPriceProduct":
+        return a.products.price - b.products.price;
+
+      case "lastProduct":
+        return (
+          new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+        );
+
+      default:
+        return 0;
+    }
+  });
+
+  // 페이지네이션 로직
   const { currentItems, currentPage, setCurrentPage, totalPages } =
-    usePagination(filterValue, 9);
+    usePagination(sortedItems, 9);
 
+  // 아이템의 찜하기 버튼 해체 시 해당 아이템 카드 사라지게 하기
   const onRemove = (id: string) => {
-    const newFilterValue = filterValue.filter((item) => item.id !== id);
-    setFilterValue(newFilterValue);
+    const newFilterValue = items.filter((item) => item.id !== id);
+    setItems(newFilterValue);
+
+    // 다 삭제한 후에 페이지를 보정해주기
+    setCurrentPage(1);
   };
 
   return (
@@ -39,10 +154,11 @@ export default function WishListItemsList() {
         <select
           name="filter"
           id="filter"
-          className="border border-gray-400 h-9 "
+          className="border border-gray-400 h-9 px-2 "
+          value={sortValue}
+          onChange={(e) => setSortValue(e.target.value)}
         >
           <option value="lastProduct">등록순</option>
-          <option value="popularProduct">인기순</option>
           <option value="highPriceProduct">가격 높은 순</option>
           <option value="lowPriceProduct">가격 낮은 순</option>
         </select>
