2f269b06094775bb1a560c6577a2bb04f67490bd
sasasak <geun02young@naver.com>
2026-05-07 15:29:51 +0900

    feat: 유저 역할 관리를 위한 UserContext 생성 - consumer와 seller 역할을 구분하기 위한 전역 상태 관리 로직 구현 (임시로 직접 역할을 지정 - 추후 DB 연결 후 변경 에정) - 하위 컴포넌트에서 쉽게 접근할 수 있도록 커스텀 훅 useUser 작성

--- src/app/mypage/layout.tsx
+++ src/app/mypage/layout.tsx
@@ -1,6 +1,9 @@
+"use client"; // Context를 사용하기 위해 추가합니다.
+
 import SideMenu from "./components/SideMenu";
 import SummaryMenu from "./components/SummaryMenu";
 import UserProfile from "./components/UserProfile";
+import { UserProvider } from "./context/UserContext";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -8,19 +11,21 @@ interface LayoutProps {
 
 export default function MyPageLayout({ children }: LayoutProps) {
   return (
-    <section className="w-full min-h-screen bg-[#FFF8F3]">
-      <div className="flex max-w-6xl mx-auto pt-32 px-4 gap-6">
-        <aside className="shrink-0">
-          <UserProfile />
-          <SideMenu />
-        </aside>
-        <main className="flex-1">
-          <div className="flex flex-col gap-6">
-            <SummaryMenu />
-            {children}
-          </div>
-        </main>
-      </div>
-    </section>
+    <UserProvider>
+      <section className="w-full min-h-screen bg-[#FFF8F3]">
+        <div className="flex max-w-6xl mx-auto pt-32 px-4 gap-6">
+          <aside className="shrink-0">
+            <UserProfile />
+            <SideMenu />
+          </aside>
+          <main className="flex-1">
+            <div className="flex flex-col gap-6">
+              <SummaryMenu />
+              {children}
+            </div>
+          </main>
+        </div>
+      </section>
+    </UserProvider>
   );
 }
