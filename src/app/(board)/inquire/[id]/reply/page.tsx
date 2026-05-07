08e31d92cc434c663b5d0be59d62087c8869539a
LDH9276 <leedh9276@naver.com>
2026-05-07 17:26:10 +0900

    fix: 1:1 문의 오류 수정

--- src/app/(board)/inquire/[id]/reply/page.tsx
+++ src/app/(board)/inquire/[id]/reply/page.tsx
@@ -1,13 +1,13 @@
 import WriteForm from '@/app/components/board/WriteForm';
-import { createNotice } from '@/api/noticeWrite';
+import { replyInquire } from '@/api/inpuireReply';
 
 export default function NoticeWritePage() {
   return (
     <div className="container mx-auto py-10">
       <WriteForm 
-        board={'1:1문의'}
-        action={createNotice} 
-        showImportantCheckbox={true} 
+        board={'1:1답변'}
+        action={replyInquire} 
+        showImportantCheckbox={false} 
       />
     </div>
   );
