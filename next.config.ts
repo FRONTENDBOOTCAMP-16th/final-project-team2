76438e7a3453e3f79a825a7f4b75e5c0821232bd
ruiwaa <yeejii08@gmail.com>
2026-05-07 17:29:37 +0900

    chore: next.config.ts 외부 이미지 불러오는 설정 구성

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,8 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'xgiayrmzgokjzwwzivzi.supabase.co',
+        protocol: "https",
+        hostname: "xgiayrmzgokjzwwzivzi.supabase.co",
       },
     ],
   },
