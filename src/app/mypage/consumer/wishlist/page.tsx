d5e96b313b1f0e30558110c1d0ad3f20b951b7fa
ruiwaa <yeejii08@gmail.com>
2026-05-08 00:44:28 +0900

    feat: 찜한 상품 페이지 suspense 적용

--- src/app/mypage/consumer/wishlist/page.tsx
+++ src/app/mypage/consumer/wishlist/page.tsx
@@ -1,5 +1,7 @@
+import { Suspense } from "react";
 import WishListItemsList from "./components/WishListItemsList";
 import { dummyOrderItems } from "@/data/dummyOrder";
+import MyPageProductSkeleton from "../../components/MypageProductSkeleton";
 
 const HAS_WISH_PRODUCTS = dummyOrderItems.length > 0;
 
@@ -8,7 +10,9 @@ export default function WishlistPage() {
     <div className="w-full bg-white pt-6 px-6 pb-11.25 ">
       <h1 className="text-2xl font-bold mb-8 sr-only">찜한 상품</h1>
       {HAS_WISH_PRODUCTS ? (
-        <WishListItemsList />
+        <Suspense fallback={<MyPageProductSkeleton count={9} />}>
+          <WishListItemsList />
+        </Suspense>
       ) : (
         <div className="text-red-500 text-center pt-3">
           <p>찜한 상품이 없습니다.</p>{" "}
