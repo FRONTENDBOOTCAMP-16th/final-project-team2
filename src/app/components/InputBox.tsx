412c49bd3b40b7c018a68197803a100aa20f7539
songha-5 <shn0518@gmail.com>
2026-05-07 16:41:35 +0900

    feat: input이 password일때 유효성 안내 문구 추가

--- src/app/components/InputBox.tsx
+++ src/app/components/InputBox.tsx
@@ -1,4 +1,6 @@
-import { useId } from "react"
+'use client'
+
+import { useId, useState } from "react"
 
 interface InputBoxProps {
   label: string
@@ -12,6 +14,25 @@ interface InputBoxProps {
 
 export default function InputBox({ label, placeholder, error, name, type = "text", value, defaultValue }: InputBoxProps) {
   const uniqueId = useId()
+  const [localValue, setLocalValue] = useState(defaultValue || "")
+  const [isFocused, setIsFocused] = useState(false)
+
+  const PASSWORD_REGEX = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*?_])^/
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLocalValue(e.target.value)
+  }
+
+  const handleFocus = () => setIsFocused(true)
+  const handleBlur = () => setIsFocused(false)
+
+  // 정규식 및 규칙
+  const isPasswordInput = type === "password" && name === "password"
+  const isValidLength = localValue.length >= 8
+  const validCheck = PASSWORD_REGEX.test(localValue)
+  
+  // 패스워드 입력시 표시
+  const showValid = isPasswordInput && localValue.length > 0 && isFocused
 
   return (
     <div className="flex flex-col">
@@ -22,10 +43,23 @@ export default function InputBox({ label, placeholder, error, name, type = "text
         type={type}
         placeholder={placeholder}
         name={name}
-        value={value}
         defaultValue={defaultValue}
+        onChange={handleChange}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
       />
       <p className="mbs-1 text-red-600" aria-live="polite" aria-hidden="true">{error}</p>
+
+      {showValid && (
+        <ul className="text-xs mbs-1 flex flex-col gap-1">
+          <li className={isValidLength ? "text-green-600 font-bold" : "text-gray-500"}>
+            {isValidLength ? "✓" : "•"} 8자 이상 입력해주세요
+          </li>
+          <li className={validCheck ? "text-green-600 font-bold" : "text-gray-500"}>
+            {validCheck ? "✓" : "•"} 영문자, 숫자, 특수기호(!,@,#,$,%,^,&,*,?,_) 포함해주세요
+          </li>
+        </ul>
+      )}
     </div>
   )
 }
