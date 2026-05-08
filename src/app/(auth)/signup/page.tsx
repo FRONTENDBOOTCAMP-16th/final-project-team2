cc56acad4fbe919900d9c7e9c65e82dcb288556b
songha-5 <shn0518@gmail.com>
2026-05-08 13:08:28 +0900

    feat: toast 추가

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -5,6 +5,8 @@ import InputBox from "@/app/components/InputBox";
 import CheckeBox from "@/app/components/CheckBox";
 import TypeRadioInput from "@/app/components/TypeRadioInput";
 import { signupAction } from "@/actions/signupAction";
+import PasswordGroup from "@/app/components/PasswordGroup";
+import SimpleToast from "@/app/components/SimpleToast";
 
 export default function SignupPage() {
   const [state, formAction] = useActionState(signupAction, null)
@@ -29,9 +31,13 @@ export default function SignupPage() {
           <div className="flex flex-col gap-3 mbs-4">
             <InputBox type="text" label="이름" name="name" placeholder="이름을 입력하세요" error={state?.errors?.name?.[0]} defaultValue={state?.name}/>
             <InputBox type="text" label="이메일" name="email" placeholder="이메일을 입력하세요" error={state?.errors?.email?.[0]} defaultValue={state?.email}/>
-            <InputBox type="text" label="핸드폰 번호" name="phone" placeholder="핸드폰 번호를 입력하세요 (010-0000-0000)" error={state?.errors?.phone?.[0]} defaultValue={state?.phone}/>
-            <InputBox type="password" label="비밀번호 설정" name="password" placeholder="비밀번호를 입력하세요" error={state?.errors?.password?.[0]} defaultValue={state?.password} />
-            <InputBox type="password" label="비밀번호 재확인" name="confirmPassword" placeholder="비밀 번호를 재입력하세요" error={state?.errors?.confirmPassword?.[0]} defaultValue={state?.confirmPassword}/>
+            <InputBox type="text" label="핸드폰 번호" name="phone" placeholder="핸드폰 번호를 입력하세요 (010-0000-0000)" error={state?.errors?.phone?.[0]} defaultValue={state?.phone} />
+            <PasswordGroup
+              passwordError={state?.errors?.password?.[0]}
+              confirmError={state?.errors?.confirmPassword?.[0]}
+              defaultPassword={state?.password}
+              defaultConfirm={state?.confirmPassword}
+            />
           </div>
 
           <div className="mbs-6 border-be pbe-6 border-[#e0e0e0]">
@@ -40,6 +46,9 @@ export default function SignupPage() {
 
           <button type="submit" className="w-full py-4 mbs-12 text-white bg-[#FF6B6B] rounded font-bold cursor-pointer">회원가입</button>
         </form>
+
+        {/* 토스트 메세지 */}
+        <SimpleToast text={state?.errors?.root?.[0] ? `아이디찾기 실패: ${state?.errors?.root?.[0]}` : undefined} trigger={state} />
       </div>
       
       <strong className="mbs-12 text-red-600 text-center">현 사이트는 실제로 운영되는 페이지가 아닙니다.<br />개인정보를 넣지 말아주세요</strong>
