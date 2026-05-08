712ba7fffcbd66e59f4354370974f7d570ac3e98
psy0821-k <p49417875@gmail.com>
2026-05-08 14:20:23 +0900

    fix: 상세페이지 작성을 위한 파일 명 수정

--- src/app/(shop)/products/[mainCategory]/[id]/page.tsx
+++ src/app/(shop)/products/[mainCategory]/[id]/page.tsx
@@ -1,7 +1,6 @@
+import { notFound } from 'next/navigation';
 import BreadCrumble from '../_components/BreadCrumble';
-import ProductInfoComponent from './_components/Product/ProductInfoComponent';
-import TabInfoComponent from './_components/Tab/TabInfoComponent';
-import { getProductDetail } from './lib/getProductDetail';
+import { isMainCategory, mainCategoryConvert } from '../lib/category';
 
 type ProductDetailPageProps = {
   params: Promise<{
@@ -13,7 +12,13 @@ type ProductDetailPageProps = {
 export default async function ProductDetailPage({ params }: ProductDetailPageProps) {
   const { mainCategory, id } = await params;
 
-  const product = await getProductDetail({ id });
+  if (!isMainCategory(mainCategory)) {
+    notFound();
+  }
+
+  const categoryLabel = mainCategoryConvert[mainCategory];
+
+  // const product = await getProductDetail({ id });
 
   return (
     <div aria-labelledby="product-detail-title" className="mt-5 mb-38 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -21,12 +26,12 @@ export default async function ProductDetailPage({ params }: ProductDetailPagePro
         제품 상세 페이지
       </h1>
 
-      <BreadCrumble categoryMap={mainCategory} />
+      <BreadCrumble category={categoryLabel} />
 
       <main>
-        <h1>{product.name}</h1>
-        <ProductInfoComponent product={product} />
-        <TabInfoComponent product={product} />
+        {/* <h1>{product.name}</h1> */}
+        {/* <ProductInfoComponent product={product} />
+        <TabInfoComponent product={product} /> */}
 
         <div className="mt-15">{/* <RecomandProduct products={products} /> */}</div>
       </main>
