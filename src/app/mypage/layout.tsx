149f660448cbf8ad8612e9c7ae7544a919151ed1
ruiwaa <yeejii08@gmail.com>
2026-05-08 13:03:52 +0900

    merge: 마이페이지 레이아웃 충돌 해결

--- src/app/mypage/layout.tsx
+++ src/app/mypage/layout.tsx
@@ -3,6 +3,7 @@
 import SideMenu from "./components/SideMenu";
 import SummaryMenu from "./components/SummaryMenu";
 import UserProfile from "./components/UserProfile";
+import MypageProviders from "./providers/MypageProviders";
 import { UserProvider } from "./context/UserContext";
 
 interface LayoutProps {
@@ -21,7 +22,7 @@ export default function MyPageLayout({ children }: LayoutProps) {
           <main className="flex-1">
             <div className="flex flex-col gap-6">
               <SummaryMenu />
-              {children}
+              <MypageProviders>{children}</MypageProviders>
             </div>
           </main>
         </div>
