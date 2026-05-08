6d0537aa9ab8091942ca1302ea70bf5984fe74f4
psy0821-k <p49417875@gmail.com>
2026-05-08 13:21:14 +0900

    fix: 제품 목록 페이지 카테고리 타입 및 정보 작성

---
+++ src/app/(shop)/products/[mainCategory]/lib/category.ts
@@ -0,0 +1,46 @@
+export type MainCategoryType = 'writing' | 'paper' | 'deco' | 'office';
+
+export type CategoryItem = {
+  label: string;
+  value: string;
+};
+
+export const mainCategories = ['writing', 'paper', 'deco', 'office'] as const;
+
+export const isMainCategory = (value: string): value is MainCategoryType => {
+  return mainCategories.includes(value as MainCategoryType);
+};
+
+export const subCategory: Record<MainCategoryType, CategoryItem[]> = {
+  writing: [
+    { label: '전체', value: '' },
+    { label: '볼펜', value: 'ballpen' },
+    { label: '만년필', value: 'fountainpen' },
+    { label: '샤프', value: 'sharp' },
+  ],
+
+  paper: [
+    { label: '전체', value: '' },
+    { label: '다이어리', value: 'diary' },
+    { label: '플래너', value: 'planner' },
+  ],
+
+  office: [
+    { label: '전체', value: '' },
+    { label: '데스크 수납/정리', value: 'desk-organizer' },
+    { label: '파일/서류보관', value: 'file-storage' },
+  ],
+
+  deco: [
+    { label: '전체', value: '' },
+    { label: '마스킹테이프', value: 'masking-tape' },
+    { label: '스티커', value: 'sticker' },
+  ],
+};
+
+export const mainCategoryConvert: Record<MainCategoryType, string> = {
+  writing: '필기구',
+  paper: '노트/다이어리',
+  deco: '데코/다꾸',
+  office: '사무/데스크용품',
+};
