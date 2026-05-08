5332df4173e860146b93ecd47e685b64a2a5702d
sasasak <geun02young@naver.com>
2026-05-08 17:44:58 +0900

    feat : 상품 관리 리스트 페이지네이션 추가 - 마이페이지 내 기존 Pagination 컴포넌트 재사용 (추후 공통 컴포넌트 확정 시 리팩토링 예정)

--- src/app/mypage/seller/products/components/SellerProductItemList.tsx
+++ src/app/mypage/seller/products/components/SellerProductItemList.tsx
@@ -1,18 +1,40 @@
+"use client";
+
+import { useState } from "react";
 import { SellerProduct } from "@/app/mypage/types/sellerOrderItems";
 import SellerProductItemCard from "./SellerProductItemCard";
+import Pagination from "../../delivery/components/Pagination";
+import { usePagination } from "@/hooks/usePagination";
 
 type Props = {
   products: SellerProduct[];
 };
 
 export default function SellerProductsList({ products }: Props) {
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 5;
+
+  const { currentItems, totalPages } = usePagination(
+    products,
+    itemsPerPage,
+    currentPage,
+  );
+
   return (
-    <ul>
-      {products.map((product) => (
-        <li key={product.id}>
-          <SellerProductItemCard product={product} />
-        </li>
-      ))}
-    </ul>
+    <div className="flex flex-col gap-6">
+      <ul>
+        {currentItems.map((product) => (
+          <li key={product.id}>
+            <SellerProductItemCard product={product} />
+          </li>
+        ))}
+      </ul>
+
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={setCurrentPage}
+      />
+    </div>
   );
 }
