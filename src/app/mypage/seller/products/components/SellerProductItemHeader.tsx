597a7a5654d7bea63ba87b7625b33eb16d54c120
sasasak <geun02young@naver.com>
2026-05-08 17:06:48 +0900

    style: 상품 관리 카드 및 헤더 디자인 수정 (와이어프레임 반영)

--- src/app/mypage/seller/products/components/SellerProductItemHeader.tsx
+++ src/app/mypage/seller/products/components/SellerProductItemHeader.tsx
@@ -1,11 +1,12 @@
 export default function SellerProductItemHeader() {
   return (
-    <div className="flex font-semibold border-b py-3 mb-2">
-      <div className="w-3/8">상품명</div>
-      <div className="w-1/8">원가</div>
-      <div className="w-1/8">총 가격</div>
-      <div className="w-1/8">할인율</div>
-      <div className="w-2/8">재고</div>
+    <div className="flex flex-row gap-x-4 px-6 py-4 font-bold text-gray-600 border-b border-gray-200 bg-gray-50/50 text-sm">
+      <div className="w-2/5">상품명</div>
+      <div className="w-[12%] text-center">원가</div>
+      <div className="w-[12%] text-center">총 가격</div>
+      <div className="w-[12%] text-center">할인율</div>
+      <div className="w-[12%] text-center">재고</div>
+      <div className="w-17"></div>
     </div>
   );
 }
