21539fe6be7121b5b4979ceb7fed2d78f6ecd419
ruiwaa <yeejii08@gmail.com>
2026-05-08 17:59:43 +0900

    feat: 상품 카테고리 그룹 추가

---
+++ src/app/mypage/consumer/wishlist/lib/categoryGroup.ts
@@ -0,0 +1,29 @@
+export const CATEGORY_GROUPS = [
+  {
+    id: "writing",
+    label: "필기구",
+
+    categories: ["필기구", "볼펜", "만년필", "샤프"],
+  },
+
+  {
+    id: "diary-note",
+    label: "노트/다이어리",
+
+    categories: ["노트/다이어리", "다이어리", "플래너"],
+  },
+
+  {
+    id: "desk",
+    label: "사무/데스크용품",
+
+    categories: ["사무/데스크용품", "데스크 수납/정리", "파일/서류보관"],
+  },
+
+  {
+    id: "deco",
+    label: "데코/다꾸",
+
+    categories: ["데코/다꾸", "마스킹테이프", "스티커"],
+  },
+];
