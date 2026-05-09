6a6d51feb7a43170c7c5ab255e8c537cd5e0e2f7
ruiwaa <yeejii08@gmail.com>
2026-05-09 17:53:53 +0900

    추가: 주문상품 데이터 페치 함수

---
+++ src/app/mypage/api/fetchOrders.ts
@@ -0,0 +1,46 @@
+import { createClient } from "../../../../utils/supabase/client";
+
+export const fetchOrders = async () => {
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) return [];
+
+  const { data, error } = await supabase
+    .from("orders")
+    .select(
+      `
+    id,
+    order_status,
+    final_price,
+    created_at,
+
+    order_items (
+      id,
+      product_id,
+      quantity,
+      unit_price,
+      item_status,
+
+      products (
+        id,
+        name,
+        thumbnail_image,
+
+        product_categories (
+          categories (
+            id,
+            name
+          )
+        )
+      )
+    )
+  `,
+    )
+    .eq("user_id", user.id);
+
+  if (error) throw error;
+  return data;
+};
