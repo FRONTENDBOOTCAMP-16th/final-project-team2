91d72efb765cc2e76c19669d87de3234e5248558
ruiwaa <yeejii08@gmail.com>
2026-05-09 21:43:51 +0900

    feat: 주문한 상품 없을 시 안내 문구 표시

--- src/app/mypage/consumer/page.tsx
+++ src/app/mypage/consumer/page.tsx
@@ -1,7 +1,4 @@
-import { dummyOrderItems } from "@/data/dummyOrder";
 import OrderList from "./orders/components/OrderList";
-
-const hasProducts = dummyOrderItems.length > 0;
 
 export default function ConsumerPage() {
   return (
@@ -10,13 +7,7 @@ export default function ConsumerPage() {
 
       <section className="flex flex-col gap-4 p-6 w-full bg-white mb-20">
         <h2 className="font-semibold px-5 sr-only">최근 주문 내역</h2>
-        {hasProducts ? (
-          <OrderList />
-        ) : (
-          <div className="text-red-500 text-center pt-3">
-            <p>주문한 상품이 없습니다.</p>
-          </div>
-        )}
+        <OrderList />
       </section>
     </div>
   );
