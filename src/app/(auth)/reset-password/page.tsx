8c2d1acb95b1131e783bfb93b3911016a7461833
psy0821-k <p49417875@gmail.com>
2026-05-09 11:33:49 +0900

    FIX:하늬님 복구파일 확인 필요

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -1,13 +1,13 @@
-'use client'
+'use client';
 
-import { resetPasswordAction } from "@/actions/resetPasswordAction"
-import InputBox from "@/app/components/InputBox"
-import PasswordGroup from "@/app/components/PasswordGroup"
-import SimpleToast from "@/app/components/SimpleToast"
-import { useActionState } from "react"
+import { resetPasswordAction } from '@/actions/resetPasswordAction';
+import InputBox from '@/app/components/InputBox';
+import PasswordGroup from '@/app/components/PasswordGroup';
+import SimpleToast from '@/app/components/SimpleToast';
+import { useActionState } from 'react';
 
 export default function ResetPasswordPage() {
-  const [state, formAction] = useActionState(resetPasswordAction, null)
+  const [state, formAction] = useActionState(resetPasswordAction, null);
 
   return (
     <div className=" bg-white w-full p-10 sm:w-160 transition-all rounded-2xl shadow-md shadow-[#c7c7c7]">
@@ -17,10 +17,31 @@ export default function ResetPasswordPage() {
 
       <form action={formAction} className="flex flex-col gap-2 mbs-10 pb-4">
         <div className="mbe-4">
-          <InputBox type="text" label="이름" name="name" placeholder="이름을 입력하세요" error={state?.errors?.name?.[0]} defaultValue={state?.name}/>
-          <InputBox type="text" label="이메일" name="email" placeholder="이메일을 입력하세요" error={state?.errors?.email?.[0]} defaultValue={state?.email}/>
-          <InputBox type="text" label="핸드폰 번호" name="phone" placeholder="핸드폰 번호를 입력하세요 (010-0000-0000)" error={state?.errors?.phone?.[0]} defaultValue={state?.phone} />
-          
+          <InputBox
+            type="text"
+            label="이름"
+            name="name"
+            placeholder="이름을 입력하세요"
+            error={state?.errors?.name?.[0]}
+            defaultValue={state?.name}
+          />
+          <InputBox
+            type="text"
+            label="이메일"
+            name="email"
+            placeholder="이메일을 입력하세요"
+            error={state?.errors?.email?.[0]}
+            defaultValue={state?.email}
+          />
+          <InputBox
+            type="text"
+            label="핸드폰 번호"
+            name="phone"
+            placeholder="핸드폰 번호를 입력하세요 (010-0000-0000)"
+            error={state?.errors?.phone?.[0]}
+            defaultValue={state?.phone}
+          />
+
           <PasswordGroup
             passwordError={state?.errors?.password?.[0]}
             confirmError={state?.errors?.confirmPassword?.[0]}
@@ -30,11 +51,13 @@ export default function ResetPasswordPage() {
         </div>
         <p className="pbs-4 text-center text-red-500 border-bs border-[#e0e0e0]">{state?.errors?.root?.[0]}</p>
 
-        <button type="submit" className="text-center w-full py-4 mbs-12 text-white bg-[#FF6B6B] rounded font-bold cursor-pointer">확인</button>
+        <button type="submit" className="text-center w-full py-4 mbs-12 text-white bg-[#FF6B6B] rounded font-bold cursor-pointer">
+          확인
+        </button>
       </form>
 
       {/* 토스트 메세지 */}
       <SimpleToast text={state?.errors?.root?.[0] ? `비밀번호 재설정 실패: ${state?.errors?.root?.[0]}` : undefined} trigger={state} />
     </div>
-  )
+  );
 }
--- src/app/components/InputBox.tsx
+++ src/app/components/InputBox.tsx
@@ -1,44 +1,64 @@
-'use client'
+'use client';
 
-import { useId, useState } from "react"
+import { useId, useState } from 'react';
 
 interface InputBoxProps {
-  label: string
-  placeholder?: string
-  name: string
-  error?: string
-  type?: "text" | "password"
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
-  onFocus?: (e: React.ChangeEvent<HTMLInputElement>) => void
-  onBlur?: (e: React.ChangeEvent<HTMLInputElement>) => void
-  defaultValue?: string
+  label: string;
+  placeholder?: string;
+  name: string;
+  error?: string;
+  type?: 'text' | 'password';
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onFocus?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onBlur?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  defaultValue?: string;
 }
 
-export default function InputBox({ label, placeholder, error, name, type = "text", onChange, onFocus, onBlur , defaultValue }: InputBoxProps) {
-  const uniqueId = useId()
-  const [localValue, setLocalValue] = useState(defaultValue || "")
-  const [isFocused, setIsFocused] = useState(false)
+export default function InputBox({ label, placeholder, error, name, type = 'text', onChange, onFocus, onBlur, defaultValue }: InputBoxProps) {
+  const uniqueId = useId();
+  const [localValue, setLocalValue] = useState(defaultValue || '');
+  const [isFocused, setIsFocused] = useState(false);
 
-  const PASSWORD_REGEX = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*?_])^/
+  const PASSWORD_REGEX = /(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*?_])/;
 
+  // onChange, Focus, Blur 이벤트 - 기본값외에 받아오는 값있으면 적용
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setLocalValue(e.target.value)
-  }
+    setLocalValue(e.target.value);
 
-  const handleFocus = () => setIsFocused(true)
-  const handleBlur = () => setIsFocused(false)
+    if (onChange) {
+      onChange(e);
+    }
+  };
+
+  const handleFocus = (e: React.FocusEvent<HTMLInputElement>) => {
+    setIsFocused(true);
+
+    if (onFocus) {
+      onFocus(e);
+    }
+  };
+
+  const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    setIsFocused(false);
+
+    if (onBlur) {
+      onBlur(e);
+    }
+  };
 
   // 정규식 및 규칙
-  const isPasswordInput = type === "password" && name === "password"
-  const isValidLength = localValue.length >= 8
-  const validCheck = PASSWORD_REGEX.test(localValue)
-  
+  const isPasswordInput = type === 'password' && name === 'password';
+  const isValidLength = localValue.length >= 8;
+  const validCheck = PASSWORD_REGEX.test(localValue);
+
   // 패스워드 입력시 표시
-  const showValid = isPasswordInput && localValue.length > 0 && isFocused
+  const showValid = isPasswordInput && localValue.length > 0 && isFocused;
 
   return (
     <div className="flex flex-col">
-      <label htmlFor={uniqueId} className="text-[#474953] font-medium">{label}</label>
+      <label htmlFor={uniqueId} className="text-[#474953] font-medium">
+        {label}
+      </label>
       <input
         className="px-3 py-2.5 mbs-1 rounded bg-[#F0F1F1]"
         id={uniqueId}
@@ -50,18 +70,18 @@ export default function InputBox({ label, placeholder, error, name, type = "text
         onFocus={handleFocus}
         onBlur={handleBlur}
       />
-      <p className="mbs-1 text-red-600" aria-live="polite" aria-hidden="true">{error}</p>
+      <p className="mbs-1 text-red-600" aria-live="polite" aria-hidden="true">
+        {error}
+      </p>
 
       {showValid && (
         <ul className="text-xs mbs-1 flex flex-col gap-1">
-          <li className={isValidLength ? "text-green-600 font-bold" : "text-gray-500"}>
-            {isValidLength ? "✓" : "•"} 8자 이상 입력해주세요
-          </li>
-          <li className={validCheck ? "text-green-600 font-bold" : "text-gray-500"}>
-            {validCheck ? "✓" : "•"} 영문자, 숫자, 특수기호(!,@,#,$,%,^,&,*,?,_) 포함해주세요
+          <li className={isValidLength ? 'text-green-600 font-bold' : 'text-gray-500'}>{isValidLength ? '✓' : '•'} 8자 이상 입력해주세요</li>
+          <li className={validCheck ? 'text-green-600 font-bold' : 'text-gray-500'}>
+            {validCheck ? '✓' : '•'} 영문자, 숫자, 특수기호(!,@,#,$,%,^,&,*,?,_) 포함해주세요
           </li>
         </ul>
       )}
     </div>
-  )
+  );
 }
