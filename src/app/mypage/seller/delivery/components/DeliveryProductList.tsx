f61f21287d59640102d690f9e2acb9293f2c3f22
psy0821-k <p49417875@gmail.com>
2026-05-09 11:18:20 +0900

    FIX: 예지님 코드 복구 확인 필요

--- src/app/mypage/seller/delivery/components/DeliveryProductList.tsx
+++ src/app/mypage/seller/delivery/components/DeliveryProductList.tsx
@@ -6,7 +6,7 @@ import Pagination from "./Pagination";
 import TabFilter from "@/app/mypage/consumer/wishlist/components/tabFilter";
 import DeliveryProductHeader from "./DeliveryProductHeader";
 import useDeliveryOrders from "@/hooks/useDeliveryOrders";
-import { useEffect } from "react";
+import { useState } from "react";
 
 const myProductIds = [
   "prod-1",
@@ -37,12 +37,17 @@ export default function DeliveryProductList() {
     useDeliveryOrders(myProductIds);
 
   // 2. 페이지네이션
-  const { currentPage, setCurrentPage, totalPages, currentItems } =
-    usePagination(sortedOrders, 4);
+  const [currentPage, setCurrentPage] = useState(1);
+  const { totalPages, currentItems } = usePagination(
+    sortedOrders,
+    5,
+    currentPage,
+  );
 
-  useEffect(() => {
+  const handleTabChangeWithReset = (id: string) => {
+    handleTabChange(id, CATEGORIES);
     setCurrentPage(1);
-  }, [sortType, setCurrentPage]);
+  };
 
   return (
     <div className="flex flex-col">
@@ -50,7 +55,7 @@ export default function DeliveryProductList() {
         <TabFilter
           items={CATEGORIES}
           selectedValue={sortType}
-          onValueChange={(id) => handleTabChange(id, CATEGORIES)}
+          onValueChange={(id) => handleTabChangeWithReset(id)}
         />
         <div>
           <DeliveryProductHeader />
