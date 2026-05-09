7f0a26135a055298fda05290bc0f3e4ac8c00a8a
ruiwaa <yeejii08@gmail.com>
2026-05-09 21:34:43 +0900

    추가: 나의 주문 상품 상태 필터링 및 탭 정렬 기능

--- src/app/mypage/consumer/wishlist/components/tabFilter.tsx
+++ src/app/mypage/consumer/wishlist/components/tabFilter.tsx
@@ -4,14 +4,12 @@ interface TabItem {
 }
 
 interface TabFilterProps {
-  items: readonly TabItem[] | TabItem[]; // 수정 가능한 배열 또는 수정 불가능한 배열이 올 수 있음
+  items: readonly TabItem[] | TabItem[];
   selectedValue: string;
   onValueChange: (id: string) => void;
   separator?: string;
 }
 
-// searchParams를 이용하여 탭 항목을 가져오는 형식으로 구현해야 되기때문에,
-// 추후 수정 필요
 export default function TabFilter({
   items,
   selectedValue,
