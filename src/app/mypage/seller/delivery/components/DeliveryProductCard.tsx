f4c308a543b7f15ced4d8a0a78474cac547c15c7
ruiwaa <yeejii08@gmail.com>
2026-05-11 22:21:58 +0900

    style: 나의 배송 관리 스타일 수정

--- src/app/mypage/seller/delivery/components/DeliveryProductCard.tsx
+++ src/app/mypage/seller/delivery/components/DeliveryProductCard.tsx
@@ -1,34 +1,40 @@
 import OrderStatusBadge from "@/app/mypage/consumer/orders/components/OrderStatusBadge";
 import { OrderItem } from "@/app/mypage/types/orderItem";
 import DeliverStatusButton from "./DeliveryStatusButton";
+import { TotalPriceFormat } from "../../../../../../utils/supabase/intl";
 
 export default function DeliveryProductCard({ order }: { order: OrderItem }) {
+  const emailPrefix = order.email?.split("@")[0];
+  console.log(order.unit_price, order.products.discount_rate);
   return (
-    <div className="flex font-semibold p-4 mb-2 border-b border-gray-300 gap-8 ">
-      {/* 주문번호 */}
-      <div className="flex w-1/9 text-center truncate">
-        <h2>{order.order_id}</h2>
+    <div className="flex font-semibold p-4 mb-2 border-b border-gray-300 gap-5 ">
+      <div className="flex w-1/10 shrink-0">
+        <h2>{order.order_id.slice(-8)}</h2>
       </div>
-
-      <div className="flex w-3/9">
+      <div className="flex w-3/10  truncate shrink-0 justify-center ">
         <h2>{order.products.name}</h2>
       </div>
-      {/* 주문자 */}
-      <div className="flex w-1/9 ">
-        <p>{order.orders.user_id}</p>
+
+      <div className="flex w-1/10  shrink-0">
+        <p>{emailPrefix}</p>
       </div>
 
-      {/* 가격 */}
-      <div className="flex w-1/9 text-center justify-center items-center">
-        <p>{order.unit_price.toLocaleString()}원</p>
+      <div className="flex w-1/10 text-center whitespace-nowrap shrink-0">
+        <p>
+          {TotalPriceFormat(
+            order.unit_price,
+            order.products.discount_rate,
+            order.quantity,
+          )}
+          원
+        </p>
       </div>
 
-      {/* 주문 수량 */}
-      <div className="flex text-center justify-center  w-1/9">
+      <div className="flex text-center justify-center  w-1/10 shrink-0">
         <p>{order.quantity}개</p>
       </div>
-      {/* 상태 */}
-      <div className="flex w-2/9 gap-2">
+
+      <div className="flex w-3/10 gap-3 shrink-0">
         <OrderStatusBadge status={order.item_status} />
         <DeliverStatusButton />
       </div>
