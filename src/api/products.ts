41525ecffd5dd52c687c21c4d2ac83a579dfd43f
songha-5 <shn0518@gmail.com>
2026-05-12 12:50:21 +0900

    feat: notFound를 next에서 사용하는것으로 대체

--- src/api/products.ts
+++ src/api/products.ts
@@ -1,11 +1,6 @@
 import { Products } from '@/app/lib/products';
-<<<<<<< HEAD
-import { createClient } from '../../utils/supabase/server';
-import { notFound } from 'next/navigation';
-=======
 import { createClient } from '@/utils/supabase/server';
-import notFound from '@/app/not-found';
->>>>>>> 8f082f3 (refactoy: utils 파일 이동)
+import { notFound } from 'next/navigation';
 
 type ProductsResponse = {
   products: Products[];
