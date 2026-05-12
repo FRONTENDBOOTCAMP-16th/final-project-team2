76072a7e633d5c71eb6e04885decf15278d9342f
ruiwaa <yeejii08@gmail.com>
2026-05-12 13:09:58 +0900

    refactor: 근영님이 수정하신 부분 커밋

--- src/app/mypage/seller/delivery/hooks/useDeliveryQuery.ts
+++ src/app/mypage/seller/delivery/hooks/useDeliveryQuery.ts
@@ -1,9 +1,9 @@
 import { fetchDelivery } from "@/app/mypage/api/fetchDelivery";
 import { useQuery } from "@tanstack/react-query";
 
-export const useDeliveryQuery = () => {
+export const useDeliveryQuery = (page: number, limit: number) => {
   return useQuery({
     queryKey: ["delivery"],
-    queryFn: fetchDelivery,
+    queryFn: () => fetchDelivery(page, limit),
   });
 };
