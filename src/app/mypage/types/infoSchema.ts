15a0bb8768b566126429f5e5f306bba4a6f955ea
sasasak <geun02young@naver.com>
2026-05-14 01:57:15 +0900

    refactor: 소비자 프로필 수정 유효성 검사 로직 분리

--- src/app/mypage/types/infoSchema.ts
+++ src/app/mypage/types/infoSchema.ts
@@ -12,3 +12,18 @@ export const sellerInfoSchema = z.object({
   name: z.string().min(1, '가게명을 작성해주세요.'),
 })
 export type SellerInfoData = z.infer<typeof sellerInfoSchema>
+
+export const profileSchema = z.object({
+  nickname: z.string().min(1, '닉네임을 입력해주세요.'),
+  phone: z
+    .string()
+    .min(1, '전화번호를 입력해주세요.')
+    .min(10, '전화번호가 너무 짧습니다.'),
+  address: z.string().min(1, '주소를 입력해주세요.'),
+  profileImage: z.string().optional(),
+  name: z.string().optional(),
+  email: z.string().optional(),
+  birthday: z.string().optional(),
+})
+
+export type ConsumerInfoData = z.infer<typeof profileSchema>
