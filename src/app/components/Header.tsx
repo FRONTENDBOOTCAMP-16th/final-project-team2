80620277cc9fb5f57a37115d1928a292025ae5f0
songha-5 <shn0518@gmail.com>
2026-05-13 13:46:21 +0900

    feat: 유저메뉴 타입 변경

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,24 +1,19 @@
 import { useAuth } from "@/hooks/useAuth"
-import NavList from "./NavList"
-
-interface NaviProps {
-  name: string
-  href: string
-  icon: string
-}
+import NavList, { NaviProps } from "./NavList"
+import { LucideSearch, LucideShoppingCart, LucideUser } from "lucide-react"
 
 export default function Header() {
   const { isLogin, handleLogout } = useAuth()
   
   {/* 유저 메뉴 */}
   const convenienceMenu = [
-    { name: '검색', onClick: () => console.log('검색 클릭'), icon: '🔍' },
-    isLogin && { name: '마이페이지', href: '/mypage/consumer', icon: '👤' },
-    { name: '장바구니', href: '/cart', icon: '🛒' },
+    { name: '검색', onClick: () => console.log('검색 클릭'), icon: <LucideSearch /> },
+    isLogin && { name: '마이페이지', href: '/mypage/consumer', icon: <LucideUser /> },
+    { name: '장바구니', href: '/cart', icon: <LucideShoppingCart /> },
     isLogin
       ? { name: '로그아웃', onClick: handleLogout, text: '로그아웃' }
       : { name: '로그인', href: '/login', text: '로그인' },
-  ].filter((item): item is NaviProps => item !== false)
-  
+  ].filter(Boolean) as NaviProps[]
+
   return <NavList label="convenience-menu" items={convenienceMenu} />
 }
