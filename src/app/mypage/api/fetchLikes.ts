2ffc6eb9c5bebca16a97be771a3b898b33cf9796
ruiwaa <yeejii08@gmail.com>
2026-05-13 17:42:29 +0900

    refactor: 찜한 상품 페이지네이션 로직 수정

--- src/app/mypage/api/fetchLikes.ts
+++ src/app/mypage/api/fetchLikes.ts
@@ -3,7 +3,8 @@ import { createClient } from '@/utils/supabase/client'
 import {
   ProductCategoriesWithCategory,
   ProductPreview,
-} from '@/app/lib/categories'
+} from '@/app/lib/Categories'
+import { CATEGORY_GROUPS } from '../consumer/wishlist/lib/categoryGroup'
 
 // categories 포함된 products (UI용)
 export type ProductCategories = {
@@ -23,16 +24,42 @@ export type ProductLikeWithProduct = ProductLike & {
   products: ProductPreview | ProductPreview[]
 }
 
-export const fetchLikes = async () => {
-  const supabase = createClient()
+export const fetchLikes = async (
+  page: number,
+  limit: number,
+  category: string = 'all',
+) => {
+  const from = (page - 1) * limit
+  const to = from + limit - 1
 
+  const supabase = createClient()
   const {
     data: { user },
   } = await supabase.auth.getUser()
 
-  if (!user) return []
+  if (!user) return { items: [], count: 0 }
 
-  const { data, error } = await supabase
+  let filteredProductIds: string[] | null = null
+
+  if (category !== 'all') {
+    const group = CATEGORY_GROUPS.find((g) => g.id === category)
+    const categoryNames = group?.categories ?? []
+
+    const { data: categoryData } = await supabase
+      .from('categories')
+      .select('id')
+      .in('name', categoryNames)
+
+    const categoryIds = categoryData?.map((c) => c.id) ?? []
+
+    const { data: categoryProducts } = await supabase
+      .from('product_categories')
+      .select('product_id')
+      .in('category_id', categoryIds)
+    filteredProductIds = categoryProducts?.map((c) => c.product_id) ?? []
+  }
+
+  let query = supabase
     .from('product_likes')
     .select(
       `
@@ -54,22 +81,34 @@ export const fetchLikes = async () => {
       )
     )
   `,
+      { count: 'exact' }, // 나눠서 가져오는 데이터의 총 개수 (표시용)
     )
     .eq('user_id', user.id)
+
+  if (filteredProductIds !== null) {
+    query = query.in('product_id', filteredProductIds)
+  }
+
+  const { data, error, count } = await query
+    .range(from, to)
     .returns<ProductLikeWithProductRaw[]>()
 
   if (error) throw error
+  if (!data) return { items: [], count: 0 }
 
   const rows = data ?? []
 
-  return rows.map((item) => {
-    const product = Array.isArray(item.products)
-      ? item.products[0]
-      : item.products
+  return {
+    items: rows.map((item) => {
+      const product = Array.isArray(item.products)
+        ? item.products[0]
+        : item.products
 
-    return {
-      ...item,
-      products: product,
-    }
-  })
+      return {
+        ...item,
+        products: product,
+      }
+    }),
+    count: count ?? 0,
+  }
 }
