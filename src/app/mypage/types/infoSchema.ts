b63902eb3c1ea2c46ac8043a3344a4d4dc7ae8a8
sasasak <geun02young@naver.com>
2026-05-13 23:00:46 +0900

    refactor : 판매자 프로필 수정 유효성 검사 로직 분리 - 기존에 info/page.tsx 내에 정의되어 있던 zod 유효성 검사 로직을 따로 types/infoSchema.ts 로 분리했습니다.

---
+++ src/app/mypage/types/infoSchema.ts
@@ -0,0 +1,13 @@
+import z from 'zod'
+
+export const sellerInfoSchema = z.object({
+  phone: z
+    .string()
+    .min(1, '전화번호를 입력해주세요.')
+    .min(10, '전화번호가 너무 짧습니다.'),
+  location: z.string().min(1, '가게 주소를 입력해주세요.'),
+  intro: z.string().min(1, '소개글을 입력해주세요.'),
+  profileImage: z.string().optional(),
+  email: z.string().optional(),
+  name: z.string().min(1, '가게명을 작성해주세요.'),
+})
