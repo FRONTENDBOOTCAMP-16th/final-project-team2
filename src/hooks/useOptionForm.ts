e1ad57359eb89ec9a37166b379893c9dc36debc4
psy0821-k <p49417875@gmail.com>
2026-05-14 10:37:55 +0900

    fix: Option => ProductOptionType 이름 변경으로 인한 변경

--- src/hooks/useOptionForm.ts
+++ src/hooks/useOptionForm.ts
@@ -1,18 +1,20 @@
 'use client'
 
 import { useState } from 'react'
-import { Option, OptionType } from '@/app/lib/products.types'
+import { OptionType, ProductOptionType } from '@/app/lib/products.types'
 
 /**
  * @param initialOptions 수정 페이지 등에서 기존 옵션을 불러올 때 사용하는 초기값
  */
 
-export default function useOptionForm(initialOptions: Option[] = []) {
+export default function useOptionForm(
+  initialOptions: ProductOptionType[] = [],
+) {
   const [optionType, setOptionType] = useState<OptionType | ''>('')
   const [optionValue, setOptionValue] = useState('')
 
   // 초기값이 배열인지 검사하여 에러를 방지합니다.
-  const [options, setOptions] = useState<Option[]>(
+  const [options, setOptions] = useState<ProductOptionType[]>(
     Array.isArray(initialOptions) ? initialOptions : [],
   )
   const [error, setError] = useState('')
