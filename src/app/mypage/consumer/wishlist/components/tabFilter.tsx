5b0616ff922ab1e64e5abf0883bc0606bbab5f22
ruiwaa <yeejii08@gmail.com>
2026-05-17 11:50:21 +0900

    style: 활성화 된 tab 텍스트 색상 수정

--- src/app/mypage/consumer/wishlist/components/tabFilter.tsx
+++ src/app/mypage/consumer/wishlist/components/tabFilter.tsx
@@ -36,7 +36,7 @@ export default function TabFilter({
               role="tab"
               className={`text-lg whitespace-nowrap transition-all ${
                 isActive
-                  ? 'font-bold text-black underline decoration-2 underline-offset-8'
+                  ? 'font-bold text-red-600 underline decoration-2 underline-offset-8'
                   : 'text-gray-500 hover:text-gray-700'
               }`}
             >
