2acde3492848fc671923b0daffa6655be16a8d36
psy0821-k <p49417875@gmail.com>
2026-05-18 23:27:36 +0900

    fix: 더미데이터 및 백업파일 삭제

--- src/app/mypage/types/coupon_backup.ts
+++
@@ -1,9 +0,0 @@
-import { Coupons, UserCoupons } from '@/app/lib/coupons.types'
-
-export interface UserCouponCombined extends UserCoupons {
-  coupon_details: Coupons
-}
-
-export interface CouponItemProps {
-  userCoupon: UserCouponCombined
-}
