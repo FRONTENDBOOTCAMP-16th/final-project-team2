0c4661fe02bf356d399d7ab63e6ba446f5a1a385
ruiwaa <yeejii08@gmail.com>
2026-05-19 01:30:04 +0900

    refactor: 상품명 h2 제거 후 span 태그로 교체

--- src/app/mypage/consumer/wishlist/page.tsx
+++ src/app/mypage/consumer/wishlist/page.tsx
@@ -3,7 +3,7 @@ import WishListItemsList from './components/WishListItemsList'
 export default function WishlistPage() {
   return (
     <div className="mb-20 w-full bg-white px-6 pt-6 pb-11.25">
-      <h1 className="sr-only mb-8 text-2xl font-bold">찜한 상품</h1>
+      <h2 className="sr-only mb-8 text-2xl font-bold">찜한 상품</h2>
 
       <WishListItemsList />
     </div>
