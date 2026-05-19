536148ee8e44de15b4723f1ad8b4cb84cd511385
LDH9276 <leedh9276@naver.com>
2026-05-19 11:17:16 +0900

    refactor: prefetch 추가

--- src/app/(board)/inquire/[id]/page.tsx
+++ src/app/(board)/inquire/[id]/page.tsx
@@ -154,6 +154,7 @@ export default async function QnaDetailPage({
 
         <Link
           href="/inquire"
+          prefetch={true}
           className="inline-block bg-slate-800 px-4 py-2 text-white hover:bg-slate-700"
         >
           목록
