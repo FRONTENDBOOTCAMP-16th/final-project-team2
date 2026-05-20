0a01b18bbb39d06434fa70e9127796c472785f46
psy0821-k <p49417875@gmail.com>
2026-05-20 13:44:37 +0900

    fix: 페이지네이션 ux개선 다음버튼 클릭시 1페이지 씩 이동

--- src/hooks/usePaginationV2.ts
+++ src/hooks/usePaginationV2.ts
@@ -1,22 +1,27 @@
 type UsePaginationParams = {
-  totalCount: number;
-  pageSize: number;
-  currentPage: number;
-  pageGroupSize?: number;
-};
+  totalCount: number
+  pageSize: number
+  currentPage: number
+  pageGroupSize?: number
+}
 
-export function usePaginationV2({ totalCount, pageSize, currentPage, pageGroupSize = 5 }: UsePaginationParams) {
-  const totalPages = Math.ceil(totalCount / pageSize);
+export function usePaginationV2({
+  totalCount,
+  pageSize,
+  currentPage,
+  pageGroupSize = 5,
+}: UsePaginationParams) {
+  const totalPages = Math.ceil(totalCount / pageSize)
 
-  const currentGroup = Math.floor((currentPage - 1) / pageGroupSize);
-  const startPage = currentGroup * pageGroupSize + 1;
-  const endPage = Math.min(startPage + pageGroupSize - 1, totalPages);
+  const currentGroup = Math.floor((currentPage - 1) / pageGroupSize)
+  const startPage = currentGroup * pageGroupSize + 1
+  const endPage = Math.min(startPage + pageGroupSize - 1, totalPages)
 
-  const isPrev = startPage > 1;
-  const isNext = endPage < totalPages;
+  const isPrev = startPage > 1
+  const isNext = endPage < totalPages
 
-  const jumpToPrev = startPage - 1;
-  const jumpToNext = endPage + 1;
+  const jumpToPrev = currentPage - 1
+  const jumpToNext = currentPage + 1
 
   return {
     currentPage,
@@ -27,5 +32,5 @@ export function usePaginationV2({ totalCount, pageSize, currentPage, pageGroupSi
     isNext,
     jumpToPrev,
     jumpToNext,
-  };
+  }
 }
