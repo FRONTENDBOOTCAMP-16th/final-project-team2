563c3022d77b14c03cda40b23cae57efb5555e66
ruiwaa <yeejii08@gmail.com>
2026-05-25 14:26:46 +0900

    style: 모바일 사이즈 가로축 스크롤 적용

--- src/app/mypage/seller/delivery/components/DeliveryProductCard.tsx
+++ src/app/mypage/seller/delivery/components/DeliveryProductCard.tsx
@@ -7,40 +7,42 @@ export default function DeliveryProductCard({ order }: { order: OrderItem }) {
   const emailPrefix = order.email?.split('@')[0]
 
   return (
-    <div className="mb-2 grid grid-cols-[2fr_2fr_1fr_1fr_1fr_2fr] gap-5 border-b border-gray-300 p-4 font-semibold">
-      <div>
-        <p className="w-32 leading-snug break-all">{order.invoice_number}</p>
-      </div>
-      <div className="min-w-0 justify-center">
-        <span className="block truncate">{order.products.name}</span>
-      </div>
+    <div className="overflow-x-auto">
+      <div className="mb-2 grid min-w-175 grid-cols-[2fr_2fr_1fr_1fr_1fr_2fr] gap-5 border-b border-gray-300 p-4 font-semibold">
+        <div>
+          <p className="w-32 leading-snug break-all">{order.invoice_number}</p>
+        </div>
+        <div className="min-w-0 justify-center">
+          <span className="block truncate">{order.products.name}</span>
+        </div>
 
-      <div>
-        <p>{emailPrefix}</p>
-      </div>
+        <div>
+          <p>{emailPrefix}</p>
+        </div>
 
-      <div className="text-center whitespace-nowrap">
-        <p>
-          {TotalPriceFormat(
-            order.unit_price,
-            order.products.discount_rate,
-            order.quantity,
-          )}
-          원
-        </p>
-      </div>
+        <div className="text-center whitespace-nowrap">
+          <p>
+            {TotalPriceFormat(
+              order.unit_price,
+              order.products.discount_rate,
+              order.quantity,
+            )}
+            원
+          </p>
+        </div>
 
-      <div className="justify-center text-center">
-        <p>{order.quantity}개</p>
-      </div>
+        <div className="justify-center text-center">
+          <p>{order.quantity}개</p>
+        </div>
 
-      <div className="flex gap-3 whitespace-nowrap">
-        <OrderStatusBadge status={order.item_status} />
-        <DeliverStatusButton
-          orderItemId={order.id}
-          currentStatus={order.item_status}
-          orderId={order.orders.id}
-        />
+        <div className="flex gap-3 whitespace-nowrap">
+          <OrderStatusBadge status={order.item_status} />
+          <DeliverStatusButton
+            orderItemId={order.id}
+            currentStatus={order.item_status}
+            orderId={order.orders.id}
+          />
+        </div>
       </div>
     </div>
   )
--- src/app/mypage/seller/delivery/components/DeliveryProductHeader.tsx
+++ src/app/mypage/seller/delivery/components/DeliveryProductHeader.tsx
@@ -1,12 +1,14 @@
 export default function DeliveryProductHeader() {
   return (
-    <div className="mb-2 grid grid-cols-[2fr_2fr_1fr_1fr_1fr_2fr] gap-5 border-b border-gray-300 p-4 font-semibold">
-      <p>주문번호</p>
-      <p>상품명</p>
-      <p>주문자</p>
-      <p>총 가격</p>
-      <p>주문 수량</p>
-      <p>주문 상태</p>
+    <div className="overflow-x-auto">
+      <div className="mb-2 grid min-w-175 grid-cols-[2fr_2fr_1fr_1fr_1fr_2fr] gap-5 border-b border-gray-300 p-4 font-semibold">
+        <p>주문번호</p>
+        <p>상품명</p>
+        <p>주문자</p>
+        <p>총 가격</p>
+        <p>주문 수량</p>
+        <p>주문 상태</p>
+      </div>
     </div>
   )
 }
