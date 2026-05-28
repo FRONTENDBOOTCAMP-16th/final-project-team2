01351089af643b8cf0561baefc04ccf96da49506
ruiwaa <yeejii08@gmail.com>
2026-05-28 13:46:32 +0900

    feat: 등록 취소 버튼 클릭 시, 모달창 추가 및 해당 값 리셋되도록 로직 구현

---
+++ src/app/mypage/seller/register/components/CancelButton.tsx
@@ -0,0 +1,63 @@
+'use client'
+
+import Modal from '@/app/components/Modal'
+import { ArrowLeft } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+
+type Props = {
+  isInputted: boolean
+  onConfirm: () => void
+}
+export default function CancelButton({ isInputted, onConfirm }: Props) {
+  const router = useRouter()
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const handleCancel = () => {
+    if (isInputted) {
+      setIsModalOpen(true)
+      return
+    }
+    router.push('/mypage/seller/products')
+  }
+  return (
+    <>
+      <button
+        type="button"
+        onClick={handleCancel}
+        className="flex items-center gap-3 bg-gray-400 p-3 font-semibold text-white hover:bg-red-500"
+      >
+        <ArrowLeft size={16} />
+        취소
+      </button>
+
+      <Modal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        title="상품 등록 취소"
+        footer={
+          <>
+            <button
+              onClick={() => setIsModalOpen(false)}
+              className="rounded-lg border px-4 py-2"
+            >
+              계속 작성
+            </button>
+            <button
+              onClick={() => {
+                onConfirm()
+                setIsModalOpen(false)
+                router.push('/mypage/seller/products')
+              }}
+              className="rounded-lg bg-black px-4 py-2 text-white"
+            >
+              취소하기
+            </button>
+          </>
+        }
+      >
+        <p>작성 중인 내용이 있습니다. 정말 취소하시겠습니까?</p>
+      </Modal>
+    </>
+  )
+}
